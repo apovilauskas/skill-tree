7e049e3d41c0ea307b8096954ef2c1636cdbc75f
Augustas P <augustaspovil@gmail.com>
2026-07-06 14:14:09 +0300

    feat set up postgres

---
+++ skill-tree/Data/SkillDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using skill_tree.Entities;
+
+namespace skill_tree.Data;
+
+public class SkillDbContext : DbContext
+{
+    public SkillDbContext(DbContextOptions<SkillDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Skill> Skills  => Set<Skill>();
+    
+}
