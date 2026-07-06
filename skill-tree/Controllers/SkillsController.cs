0350eace40876f0a51c3f428e1c69b17066f6843
Augustas P <augustaspovil@gmail.com>
2026-07-06 18:27:39 +0300

    feat add controller with basic get & post, test them with requests

---
+++ skill-tree/Controllers/SkillsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using skill_tree.Data;
+using skill_tree.Entities;
+
+namespace skill_tree.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SkillsController : ControllerBase
+{
+    private SkillDbContext _context;
+    
+    public SkillsController(SkillDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllSkills()
+    {
+        var skills = await _context.Skills.ToListAsync();
+        return Ok(skills);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateSkill(Skill skill)
+    {
+        _context.Skills.Add(skill);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetAllSkills), new { id = skill.Id }, skill);
+    }
+}
