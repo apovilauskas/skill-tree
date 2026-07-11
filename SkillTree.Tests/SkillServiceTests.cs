383c4512882c62290854b39749e4324f74a4689f
Augustas P <augustaspovil@gmail.com>
2026-07-11 12:13:18 +0300

    test: setup xUnit project with initial zero logs test and renamed project to PascalCase

---
+++ SkillTree.Tests/SkillServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using skill_tree.Entities;
+using skill_tree.Service;
+
+namespace skill_tree.tests;
+
+public class SkillServiceTests
+{
+    [Fact]
+    public void Progress_WithZeroLogs_ShouldReturnZero()
+    {
+        Skill testSkill = new Skill
+        {
+            SkillLogs = new List<SkillLog>(),
+            Metric = "hour",
+            Target = 40
+        };
+        
+        SkillTreeService skillTreeService = new SkillTreeService();
+        double progress = skillTreeService.Progress(testSkill);
+        Assert.Equal(0, progress);
+    }
+}
